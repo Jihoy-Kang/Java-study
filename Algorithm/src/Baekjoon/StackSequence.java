64626697967989f7677e92e34455d7c7c481a315
Jihyo Kang <luxuryjihyo@naver.com>
2022-06-12 14:36:09 +0900

    12 June 2022

--- Algorithm/src/Baekjoon/StackSequence.java
+++ Algorithm/src/Baekjoon/StackSequence.java
@@ -2,13 +2,25 @@ package Baekjoon;
 
 import java.lang.module.FindException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
+import java.io.*;
 
 public class StackSequence {
-    public static void main(String[] args) {
-        int[] arr = new int[]{1,2,5,3,4};
-        ArrayList<Integer> result = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine()); //Int // 첫번째 n값 입력
+        int[] arr = new int[n]; // 입력값 넣을 배열 생성
+
+        for(int i = 0; i < n;i++){ // 배열에 입력값 넣기
+            arr[i] = Integer.parseInt(bf.readLine());
+        }
+
+        //ArrayList<Integer> result = new ArrayList<>(); // 제대로 출력이 되는 확인하기 위한 어레이리스트(없어도 됨)
+        ArrayList<String> print = new ArrayList<>(); // +,-, NO 넣을 어레이리스트
+        Stack<Integer> stack = new Stack<>(); //
+        //System.out.println(Arrays.toString(arr));
+
         int index = 0;
         int num = 2;
         //첫번째 숫자  나올때까지 push
@@ -17,22 +29,37 @@ public class StackSequence {
         //스택[0]에 있으면 다시 pop
 
         stack.push(1);
-
+        print.add("+");
         while(!stack.isEmpty()){
             if(stack.search(arr[index]) == -1){
                 stack.push(num);
+                print.add("+");
                 num++;
             }else if(stack.search(arr[index]) != -1 && stack.peek() != arr[index] ){
-                System.out.println("No");
+                print.add("NO");
                 break;
             }
             if(arr[index] == stack.peek()){
-                result.add(stack.pop());
+                //result.add(stack.pop());
+                stack.pop(); // result 활성화시키면 이거 비활성화해야함
+                print.add("-");
                 index++;
             }
+            if(stack.isEmpty() && num <= n){
+                stack.push(num);
+                print.add("+");
+                num++;
+            }
         }
-        System.out.println(stack);
-        System.out.println(result);
+        if(print.contains("NO")){
+            System.out.println("NO");
+        }else{
+            for(String s : print){
+                System.out.println(s);
+            }
+        }
+        //System.out.println(result);
+
     }
 }
 /*      stack.push(1);
---
+++ Algorithm/src/Baekjoon/StringSet.java
@@ -0,0 +1,63 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class StringSet {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        HashSet<String> setN = new HashSet<>();
+        HashSet<String> setM = new HashSet<>();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+
+        for(int i = 0; i < N ;i++) setN.add(br.readLine());
+        /*for(int j = 0; j < M ;j++) setM.add(br.readLine());
+        setN.retainAll(setM); // 교집합
+        System.out.println(setN.size());*/ // 이게 더 빠를거 같은데.......
+
+        for(int j = 0; j < M ;j++) {
+            if(setN.contains(br.readLine())){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
+
+
+   /* HashSet<Integer> ss = new HashSet();	// 1 2 3 4
+    HashSet<Integer> ss2 = new HashSet();	// 3 4 5 6
+    HashSet<Integer> ss3 = new HashSet();	// 1 2
+
+		ss.add(1);
+                ss.add(2);
+                ss.add(3);
+                ss.add(4);
+
+                ss2.add(3);
+                ss2.add(4);
+                ss2.add(5);
+                ss2.add(6);
+
+                ss3.add(1);
+                ss3.add(2);
+
+		ss.addAll(ss2);	// (1 2 3 4) ∪ (3 4 5 6) = (1 2 3 4 5 6) 합집합
+		for(int n : ss) System.out.print(n + " ");
+		System.out.println();
+
+		ss.retainAll(ss2);	// (1 2 3 4) ∩ (3 4 5 6) = (3 4) 교집합
+		for(int n : ss) System.out.print(n + " ");
+		System.out.println();
+
+		ss.removeAll(ss2);	// (1 2 3 4) - (3 4 5 6) = (1 2) 차집합
+		for(int n : ss) System.out.print(n + " ");
+		System.out.println();
+*/
