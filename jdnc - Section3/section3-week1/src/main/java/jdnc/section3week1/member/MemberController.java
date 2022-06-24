2923218e9af74191e180d418bf9866a286c3348a
Jihyo Kang <luxuryjihyo@naver.com>
2022-06-24 13:55:01 +0900

    24 June 2022 3

--- jdnc - Section3/section3-week1/src/main/java/jdnc/section3week1/member/MemberController.java
+++ jdnc - Section3/section3-week1/src/main/java/jdnc/section3week1/member/MemberController.java
@@ -3,31 +3,37 @@ package jdnc.section3week1.member;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/v1/members")
+@Validated
 public class MemberController {
     @PostMapping
-    public ResponseEntity postMember(@RequestParam("email") String email,
-                                     @RequestParam("name") String name,
-                                     @RequestParam("phone") String phone){
-        Map<String, String> map = new HashMap<>()();
+    public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto){
 
-        map.put("email",email);
-        map.put("name",name);
-        map.put("phone",phone);
-
-        return new ResponseEntity<>(map, HttpStatus.CREATED);
+        return new ResponseEntity<>(memberPostDto, HttpStatus.CREATED);
     }
+
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(@PathVariable("member-id") @Min(1) long memberId,
+                                     @Valid @RequestBody MemberPatchDto memberPatchDto) {
+        memberPatchDto.setMemberId(memberId);
+        memberPatchDto.setName("홍길동");
+        return new ResponseEntity<>(memberPatchDto, HttpStatus.OK);
+    }
+
     @GetMapping("/{member-id}")
     public ResponseEntity getMember(@PathVariable("member-id")long memberId){
         System.out.println("# memberId = " + memberId);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<Map>(HttpStatus.OK);
     }
 
     @GetMapping
@@ -35,5 +41,10 @@ public class MemberController {
         System.out.println("# get Members");
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteMember(@PathVariable("member-id")long memberId){
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
