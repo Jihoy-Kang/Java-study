93de669ac06f787e13b2ff9ce9ecf769ca21e8f4
Jihyo Kang <luxuryjihyo@naver.com>
2022-07-01 11:12:08 +0900

    01 July 2022

---
+++ jdbcPractice/src/test/java/jdnc/jdbcPractice/connection/JdbcPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package jdnc.jdbcPractice.connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
