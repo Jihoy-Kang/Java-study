28221abc796d0f087ad9341ace9e0b64b92d776b
Jihyo Kang <luxuryjihyo@naver.com>
2022-07-03 18:27:13 +0900

    03 July 2022

--- jdbcPractice/src/test/java/jdnc/jdbcPractice/repository/MemberRepositoryV1Test.java
+++ jdbcPractice/src/test/java/jdnc/jdbcPractice/repository/MemberRepositoryV1Test.java
@@ -1,20 +1,38 @@
 package jdnc.jdbcPractice.repository;
 
+import com.zaxxer.hikari.HikariDataSource;
 import jdnc.jdbcPractice.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import java.sql.Driver;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static jdnc.jdbcPractice.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-class MemberRepositoryV0Test {
+class MemberRepositoryV1Test {
 
-    MemberRepositoryV0 repository = new MemberRepositoryV0();
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach(){
+        //기본 드라이버매니저를 통한 항상 새로운 커넷션 획득
+        //DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME,PASSWORD);
+
+        //커넥션풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        repository = new MemberRepositoryV1(dataSource);
+    }
 
     @Test
     void crud() throws SQLException {
@@ -36,5 +54,6 @@ class MemberRepositoryV0Test {
         repository.delete(member.getMemberId());
         Assertions.assertThatThrownBy(()->repository.findById(member.getMemberId()))
                 .isInstanceOf(NoSuchElementException.class);
+
     }
 }
